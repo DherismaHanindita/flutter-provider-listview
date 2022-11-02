c0762815635bcbd38add0e3f8d2e4aa244f03837
= <=>
2022-11-02 15:29:13 +0700

    dherisma

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Listview dengan Provider
 
-Challenge untuk hacktoberfest :
+<p>Nama    : Dherisma Hanindita Utami
+<p>NIM     : 2041720018
+<p>No      : 06 
+<p>Kelas   : TI 3H
 
-1. Tambahkan Fitur untuk Nambah Task baru sesuai dengan input user di page tambah task.
-2. Tambahkan fitur untuk validasi input di tambah task.
-3. Tambahkan fitur untuk disable button tambah task jika validasi input masih error.
+<p>Jawaban
+
+1. Tambahkan fitur untuk validasi input di tambah task.
+<img src = './gambar/8.PNG'>
+2. Tambahkan fitur untuk disable button tambah task jika validasi input masih error.
+<img src = './gambar/8.PNG'>
+3. Tambahkan fitur swipe left delete task
+<img src = './gambar/6.jpeg'>
+4. Tambahkan fitur swipe right go to edit task page
+<img src = './gambar/4.jpeg'>
+5. Tambahkan fitur edit task.
+<img src = './gambar/7.jpeg'>
+6. Tambahkan fitur validasi input di edit task.
+<img src = './gambar/5.jpeg'>
+7. Tambahkan fitur disable button edit task jika validasi input masih error.
+<img src = './gambar/5.jpeg'>
--- lib/page/listpage.dart
+++ lib/page/listpage.dart
@@ -4,15 +4,29 @@ import 'package:provider/provider.dart';
 import '../models/task.dart';
 import '../service/tasklist.dart';
 
-class MyListPage extends StatelessWidget {
+class MyListPage extends StatefulWidget {
   const MyListPage({super.key});
 
   @override
+  State<MyListPage> createState() => _MyListPageState();
+}
+
+class _MyListPageState extends State<MyListPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    context.read<Tasklist>().fetchTaskList();
+  }
+
+  final dc = DismissDirection.endToStart;
+
+  @override
   Widget build(BuildContext context) {
-    List<Task> taskList = context.watch<Tasklist>().taskList;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dynamic Listview dengan provider"),
+        centerTitle: true,
+        title: const Text("UTS - Maulana Bintang Irfansyah - 3H"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -20,10 +34,52 @@ class MyListPage extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                itemCount: taskList.length,
+                itemCount: context.watch<Tasklist>().taskList.length,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(taskList[index].name),
+                  var task = context.watch<Tasklist>().taskList[index];
+                  return Dismissible(
+                    key: UniqueKey(),
+                    background: Container(
+                      color: Colors.blue,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.inbox_rounded, color: Colors.white),
+                            Text('bergerak Edit data',
+                                style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                    ),
+                    secondaryBackground: Container(
+                      color: Colors.red,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Icon(Icons.delete, color: Colors.white),
+                            Text('bergerak Hapus data',
+                                style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                    ),
+                    onDismissed: (direction) {
+                      if (direction == DismissDirection.startToEnd) {
+                        Navigator.pushNamed(context, "/updateTask");
+                        if (!mounted) return;
+                        context.read<Tasklist>().fetchTaskList();
+                        print("edit data");
+                      } else {
+                        context.read<Tasklist>().deleteTask(task);
+                      }
+                    },
+                    child: ListTile(
+                      title:
+                          Text(context.watch<Tasklist>().taskList[index].name),
+                    ),
                   );
                 },
               ),
@@ -32,8 +88,11 @@ class MyListPage extends StatelessWidget {
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/addTask");
+                    onPressed: () async {
+                      // context.read<Tasklist>().addTask();
+                      await Navigator.pushNamed(context, "/addTask");
+                      if (!mounted) return;
+                      context.read<Tasklist>().fetchTaskList();
                     },
                     child: const Text("Halaman Tambah"),
                   ),
@@ -46,3 +105,23 @@ class MyListPage extends StatelessWidget {
     );
   }
 }
+
+Widget buildSwipeActionLeft() => Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      color: Colors.yellow[700],
+      child: Icon(
+        Icons.archive_sharp,
+        color: Colors.white,
+      ),
+    );
+
+Widget buildSwipeActionRight() => Container(
+      alignment: Alignment.centerRight,
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      color: Colors.red[700],
+      child: Icon(
+        Icons.delete_forever,
+        color: Colors.white,
+      ),
+    );
